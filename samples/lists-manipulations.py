cf4447aa52c7a5e8ebc9abd25f80ac3f885f91ba
Thierry DECKER <mail@thierry-decker.com>
2018-01-22 06:20:17 +0100

    Updated lists-manipulations

--- samples/lists-manipulations.py
+++ samples/lists-manipulations.py
@@ -28,21 +28,28 @@ def main():
 
     # Affiche le nombre de pommes
     print(fruits.count('pomme'))
+
     # Affiche le nombre de coing
     print(fruits.count('coing'))
+
     # Affiche la postion de la premièere occurence de banane
     fruits.index('banane')
+
     # Affiche la postion de la premièere occurence de banane à partie de l'index 4
     fruits.index('banane', 4)
+
     # Renverse la liste et l'affiche
     fruits.reverse()
     print(fruits)
+
     # Ajoute une élément à la fin
     fruits.append('raisin')
     print(fruits)
+
     # Trie la liste et l'affiche
     fruits.sort()
     print(fruits)
+
     # Récupère le dernier élément et l'enlève de la liste
     fruits.pop()
     print(fruits)
