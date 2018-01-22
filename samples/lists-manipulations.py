9fc2612fe30760a2b6a8ab40983d4198e7c61748
Thierry DECKER <mail@thierry-decker.com>
2018-01-22 06:17:58 +0100

    Added lists-manipulations

---
+++ samples/lists-manipulations.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def main():
+    """The entry point of the script
+
+    This function will act as an entry point for the script.
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    Raises:
+        None
+
+    """
+    fruits = [
+        'orange',
+        'pomme',
+        'poire',
+        'banane',
+        'kiwi',
+        'pomme',
+        'banane']
+
+    # Affiche le nombre de pommes
+    print(fruits.count('pomme'))
+    # Affiche le nombre de coing
+    print(fruits.count('coing'))
+    # Affiche la postion de la premièere occurence de banane
+    fruits.index('banane')
+    # Affiche la postion de la premièere occurence de banane à partie de l'index 4
+    fruits.index('banane', 4)
+    # Renverse la liste et l'affiche
+    fruits.reverse()
+    print(fruits)
+    # Ajoute une élément à la fin
+    fruits.append('raisin')
+    print(fruits)
+    # Trie la liste et l'affiche
+    fruits.sort()
+    print(fruits)
+    # Récupère le dernier élément et l'enlève de la liste
+    fruits.pop()
+    print(fruits)
+
+
+if __name__ == '__main__':
+    #
+    # Call the main() function
+    #
+    main()
