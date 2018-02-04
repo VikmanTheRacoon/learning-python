80b24dbc501313ec89f42a67b35189ba9cc3f4f7
Thierry DECKER <mail@thierry-decker.com>
2018-02-04 18:37:47 +0100

    Added lifo queue

---
+++ samples/queues_lifo.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import threading
+import time
+import queue
+
+
+def my_subscriber(my_queue):
+    while not my_queue.empty():
+        item = my_queue.get()
+        if item is None:
+            break
+        print('{} Removed {} from queue'.format(threading.current_thread().getName(), item))
+        my_queue.task_done()
+        time.sleep(1)
+
+
+def main():
+    my_queue = queue.LifoQueue()
+    for i in range(10):
+        print('Put {} in my_queue'.format(i))
+        my_queue.put(i)
+    print('my_queue populated')
+    threads = []
+    for i in range(4):
+        t = threading.Thread(target=my_subscriber, args=(my_queue,))
+        t.start()
+        threads.append(t)
+    my_queue.join()
+    print('Queue is empty')
+    for t in threads:
+        t.join()
+
+
+if __name__ == '__main__':
+    main()
