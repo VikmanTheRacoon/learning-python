f2cda4d3ef66f1010d0e5cbeb85a68311b11628a
Thierry DECKER <mail@thierry-decker.com>
2018-02-05 15:35:14 +0100

    Updated queues_empty_full

--- samples/queues_empty_full.py
+++ samples/queues_empty_full.py
@@ -8,24 +8,23 @@ import queue
 
 def my_subscriber(my_queue):
     time.sleep(1)
-    while not my_queue.empty():
-        item = my_queue.get()
-        if item is None:
-            break
-        print('{} Removed {} from queue'.format(threading.current_thread().getName(), item))
-        my_queue.task_done()
+    while not my_queue.full():
+        my_queue.put(1)
+        print('{} Appended 1 to queue'.format(threading.current_thread().getName()))
+        time.sleep(1)
 
 
 def main():
-    my_queue = queue.Queue()
-    for i in range(5):
-        print('Put {} in my_queue'.format(i))
-        my_queue.put(i)
-    print('my_queue populated')
-    t = threading.Thread(target=my_subscriber, args=(my_queue,))
-    t.start()
+    my_queue = queue.Queue(maxsize=5)
+
+    threads = []
+    for i in range(4):
+        t = threading.Thread(target=my_subscriber, args=(my_queue,))
+        t.start()
+        threads.append(t)
     my_queue.join()
-    print('Queue is now empty')
+    for t in threads:
+        t.join()
 
 
 if __name__ == '__main__':
