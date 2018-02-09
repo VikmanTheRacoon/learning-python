6d80802628bc30c589ab72d4edee00702708036f
Thierry DECKER <mail@thierry-decker.com>
2018-02-09 18:07:49 +0100

    Added multiprocessing_3

---
+++ samples/multiprocessing_3.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import multiprocessing
+import time
+import random
+
+
+def worker_process():
+    print("Process started: {}".format(multiprocessing.current_process().pid))
+    time.sleep(random.randint(3, 6))
+    print("Process terminated: {}".format(multiprocessing.current_process().pid))
+
+
+def main():
+    print("Main process: {}".format(multiprocessing.current_process().pid))
+    my_process = multiprocessing.Process(target=worker_process)
+    my_process.daemon = True
+    my_process.start()
+    print("We can carry on as per usual and our daemon will continue to execute")
+    time.sleep(10)
+    print("Main process terminated: {}".format(multiprocessing.current_process().pid))
+
+
+if __name__ == '__main__':
+    main()
